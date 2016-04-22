e996bdc819622b267918ca9b7fd4e1b1f99a631b
Lukas Rieder <lukas.rieder@iml.unibe.ch>
2016-04-22 17:18:58 +0200

    Merge with device owner capabilites from oddmouse
    - only enable "forced" lock task mode if possible and necessary
    - replace events with `isInLockTaskMode()`, since polling seems the only
      reliable way of determining the state anyway
    - code cleanup

--- www/LockTask.js
+++ www/LockTask.js
@@ -1,52 +1,27 @@
-(function(window, undefined)
-{
+(function (window, undefined) {
   'use strict';
 
   var LockTask = {
-    _listener: {},
-    startLockTask: function (successCallback, errorCallback, opts) {
-      cordova.exec(successCallback, errorCallback, "LockTask", "START_LOCK_TASK", []);
+    startLockTask: function (successCallback, errorCallback, adminClassName) {
+      cordova.exec(successCallback, errorCallback, "LockTask", "START_LOCK_TASK", adminClassName ? [adminClassName] : []);
     },
+
     stopLockTask: function (successCallback, errorCallback) {
       cordova.exec(successCallback, errorCallback, "LockTask", "STOP_LOCK_TASK", []);
     },
+
     isLockTaskSupported: function (successCallback, errorCallback) {
       cordova.exec(successCallback, errorCallback, "LockTask", "IS_LOCK_TASK_SUPPORTED", []);
     },
-    on: function (event, callback, scope) {
-      if (!this._listener[event]) {
-        this._listener[event] = [];
-      }
-      var item = [callback, scope || window];
-      this._listener[event].push(item);
-    },
-    off: function (event, callback) {
-      var listener = this._listener[event];
-      if (!listener) return;
-      for (var i = 0; i < listener.length; i++) {
-        var fn = listener[i][0];
-        if (fn == callback) {
-          listener.splice(i, 1);
-          break;
-        }
-      }
-    },
-    fireEvent: function (event) {
-      var args = Array.apply(null, arguments).slice(1),
-        listener = this._listener[event];
 
-      if (!listener) return;
-
-      for (var i = 0; i < listener.length; i++) {
-        var fn = listener[i][0], scope = listener[i][1];
-        fn.apply(scope, args);
-      }
-    }
+    isInLockTaskMode: function (successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, "LockTask", "IS_IN_LOCK_TASK_MODE", []);
+    },
   };
 
   cordova.addConstructor(function() {
     window.LockTask = LockTask;
+
     return window.LockTask;
   });
-
 })(window);
